f13888011d910088778fe73a5b485d782db115a6
Mahefa <fdsfs>
2020-04-26 17:59:23 +0300

    fdgre

--- src/Engine/Engine.ts
+++ src/Engine/Engine.ts
@@ -5,4 +5,39 @@ export default class Engine {
     return 
   }
 
+  public static fdgdf(year: number, mounth: number, day: number) {
+    return 
+  }
+
+}
+
+const AnaranandroMalagasy = {
+    "2019-07-22": "Lohalakaosy",
+    "2019-07-23": "Tsidina",
+    "2019-07-24": "Ranomaso",
+    "2019-07-25": "Tratrany",
+    "2019-07-26": "Sandramody",
+    "2019-07-27": "Adalomanara",
+    "2019-07-28": "Faradalo",
+    "2019-07-29": "Lohalohotsy",
+    "2019-07-30": "Faralohotsy",
+    "2019-07-31": "Vavalamadina",
+    "2019-08-01": "Handrona",
+    "2019-08-02": "Soria",
+    "2019-08-03": "Adaorovokatra",
+    "2019-08-04": "Faralahaka",
+    "2019-08-05": "Ravina",
+    "2019-08-06": "Vahany",
+    "2019-08-07": "Vavasorotany",
+    "2019-08-08": "Kokony",
+    "2019-08-09": "Hifiny",
+    "2019-08-10": "Alasata",
+    "2019-08-11": "Haravoana",
+    "2019-08-12": "Soambola",
+    "2019-08-13": "Alaova",
+    "2019-08-14": "Alakaforo",
+    "2019-08-15": "Admizana",
+    "2019-08-16": "Kididy",
+    "2019-08-17": "Alakarabo",
+    "2019-08-18": "Fanagnorena"
 }
