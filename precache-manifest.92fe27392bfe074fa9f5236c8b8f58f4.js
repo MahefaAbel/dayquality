1fa181e762ae9df027ff2fa2372546d32b55e02c
Mahefa Abel <abelmahefa@gmail.com>
2021-10-01 22:20:29 +0300

    Updates

---
+++ precache-manifest.92fe27392bfe074fa9f5236c8b8f58f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7981cce7707da39e7a3479a055c1f0f4",
+    "url": "/dayquality/index.html"
+  },
+  {
+    "revision": "a92401ac0dd3591f2685",
+    "url": "/dayquality/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "6de18738ff35c333a67a",
+    "url": "/dayquality/static/css/main.cbc27643.chunk.css"
+  },
+  {
+    "revision": "a92401ac0dd3591f2685",
+    "url": "/dayquality/static/js/2.314819ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dayquality/static/js/2.314819ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6de18738ff35c333a67a",
+    "url": "/dayquality/static/js/main.da7dfc56.chunk.js"
+  },
+  {
+    "revision": "65d09ba084028f1831a3",
+    "url": "/dayquality/static/js/runtime-main.c1be4126.js"
+  }
+]);
